3d326fa60df5fdc263ae93831f86e609622d9bef
DimaPysmennyi <gdimon4ik10@gmail.com>
2025-01-25 23:21:58 +0200

    added context

--- src/shared/App.tsx
+++ src/shared/App.tsx
@@ -2,19 +2,39 @@ import { Layout } from "./Layout/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { PostPage } from "../pages/PostPage/PostPage";
 import { PostListPage } from "../pages/PostListPage/PostListPage";
+import { IPost } from "./PostCard/PostCard";
+import { createContext, useState } from "react";
+
+interface IPostContext {
+    likedPosts: IPost[];
+    addLikedPost: (post: IPost) => void;
+}
+
+const initialValue: IPostContext = {likedPosts: [], addLikedPost: (post: IPost) => {}};
+export const postContext = createContext<IPostContext>(initialValue);
 
 export function App(){
+    const [likedPosts, setLikedPosts] = useState<IPost[]>([]);
+
+    function addLikedPost(post: IPost){
+        let newLikedPosts = [...likedPosts, post];
+        setLikedPosts(newLikedPosts);
+    }
+
     return (
         <div>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout></Layout>}>
-                        <Route path="/posts" element={<PostListPage></PostListPage>}></Route>
-                        <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
-                    </Route>
-                </Routes>
+            <postContext.Provider value={{likedPosts: likedPosts, addLikedPost: addLikedPost}}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Layout></Layout>}>
+                            <Route path="/posts" element={<PostListPage></PostListPage>}></Route>
+                            <Route path="/post/:id" element={<PostPage></PostPage>}></Route>
+                        </Route>
+                    </Routes>
 
-            </BrowserRouter>
+                </BrowserRouter>
+            </postContext.Provider>
+
         </div>
     )
 }
--- src/shared/PostCard/PostCard.tsx
+++ src/shared/PostCard/PostCard.tsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import './PostCard.css';
 import { Link } from "react-router-dom";
+import { postContext } from "../App";
 
-interface IProps{
+
+export interface IPost{
     id: number,
     headline: string,
     desc: string,
@@ -10,24 +12,31 @@ interface IProps{
     author: string
 }
 
-export function PostCard(props: IProps){
+export function PostCard(props: IPost){
     let [likes, setLikes] = useState(0)
     let [disabled, setDisabled] = useState(false)
+    let likedPosts = useContext(postContext);
+
     function addLikes(){
         setLikes(likes+1);
+        likedPosts.addLikedPost(props);
         setDisabled(true);
     }
 
+    console.log(likedPosts);
+
     return (
-        <Link to={`/post/${props.id}`}>
+        
             <div className='post'>
-                <div className="img">
-                    <img src={props.src} alt="" />
-                </div>
-                <div className="headlines">
-                    <h2>{props.headline.slice(0, 23)}...</h2>
-                    <p>{props.author}</p>
-                </div>
+                <Link to={`/post/${props.id}`}>
+                    <div className="img">
+                        <img src={props.src} alt="" />
+                    </div>
+                    <div className="headlines">
+                        <h2>{props.headline.slice(0, 23)}...</h2>
+                        <p>{props.author}</p>
+                    </div>
+                </Link>
                 <div className="desc">
                     <p>{props.desc.slice(0, 100)}...</p>
                     <div className="button-box">
@@ -36,7 +45,6 @@ export function PostCard(props: IProps){
                     </div>
                 </div>
             </div>
-        </Link>
 
         
     )
